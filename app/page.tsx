3588360180d3271d32b72a5a899c1e6183962592
Ivan <vanyok.korobka@ukr.net>
2024-01-07 22:21:10 +0200

    create Hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-import Image from 'next/image'
+import Hero from '@/components/main/Hero'
 
 export default function Home() {
   return (
-    <main>Space portfolio</main>
+    <main className='h-full w-full'>
+      <div className='flex h-[850px] flex-col gap-20'>
+        <Hero />
+      </div>
+    </main>
   )
 }
