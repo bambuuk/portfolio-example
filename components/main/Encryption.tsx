37c1b232c97ac4b77bdb27032b9c628b1d9e7bcf
Ivan <vanyok.korobka@ukr.net>
2024-01-10 23:46:16 +0200

    prepare adaptive for Encryption

--- components/main/Encryption.tsx
+++ components/main/Encryption.tsx
@@ -6,11 +6,11 @@ import Image from 'next/image';
 
 const Encryption = () => {
   return (
-    <div className='flex flex-row relative items-center justify-center w-full min-h-screen h-full'>
-      <div className='absolute w-auto h-auto top-0 z-[5]'>
+    <section className='flex flex-row relative items-center justify-center w-full min-h-screen h-full'>
+      <div className='absolute w-auto h-auto top-[60px] sm:top-[50px] lg:top-0 z-[5]'>
         <motion.div
           variants={slideInFromTop}
-          className='text-[40px] font-medium text-center text-gray-200'
+          className='text-[25px] sm:text-[40px] font-medium text-center text-gray-200'
         >
           Performance
           <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
@@ -44,8 +44,8 @@ const Encryption = () => {
         </div>
       </div>
 
-      <div className='absolute z-[20] bottom-[10px] px-[5px]'>
-        <div className='cursive text-[20px] font-medium text-center text-gray-300'>
+      <div className='absolute z-[20] bottom-[80px] sm:bottom-[50px] lg:bottom-[10px] px-[5px]'>
+        <div className='cursive text-lg sm:text-[20px] font-medium text-center text-gray-300'>
           Secure your data with end-to-end encryption
         </div>
       </div>
@@ -61,7 +61,7 @@ const Encryption = () => {
           src="/encryption.webm"
         />
       </div>
-    </div>
+    </section>
   )
 }
 
